97f65977058767cdeb2340ea17cbe4ab55a18fda
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-05 18:08:48 +1000

    Time: 117 ms (23.45%), Space: 13.9 MB (35.10%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,20 @@
+nums = [{1: 'I', 4: 'IV', 5: 'V', 9: 'IX'},
+        {1: 'X', 4: 'XL', 5: 'L', 9: 'XC'},
+        {1: 'C', 4: 'CD', 5: 'D', 9: 'CM'}, {1:'M'}]
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        decomposed_num = [int(i) for i in str(num)][-1::-1]
+        #print(decomposed_num)
+        s = ''
+        i = 0
+        for n in decomposed_num:
+            if n in [4, 9]:
+                s = nums[i][n] + s
+            elif 0 < n < 4:
+                s = ''.join([nums[i][1] for _ in range(n)]) + s
+            elif 5 <= n < 9:
+                s = nums[i][5] + ''.join([nums[i][1] for _ in range(n-5)]) + s
+                
+            i += 1
+        return s
