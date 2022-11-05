0d2d854734dd15e135c1775a263516e763fb9499
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-05 16:48:46 +1000

    Time: 122 ms (12.53%), Space: 13.8 MB (76.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,7 @@
 I, X, C, M = 'I', 'X', 'C', 'M'
 V, L, D = 'V', 'L', 'D'
 nums = {I: 1, V: 5, X:10, L:50, C:100, D:500, M: 1000}
+
 class Solution:
 
     def romanToInt(self, s: str) -> int:
@@ -11,7 +12,7 @@ class Solution:
             cur_num = s[i]
             next_num = s[i+1] if i<len_s-1 else None
 
-            if cur_num in [I, X, C] and next_num:
+            if next_num and cur_num in [I, X, C]:
                 if (cur_num == I and next_num in [V, X]) or\
                    (cur_num == X and next_num in [L, C]) or\
                    (cur_num == C and next_num in [D, M]):
