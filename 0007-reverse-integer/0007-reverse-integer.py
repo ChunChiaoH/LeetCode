1b46da9edf886a01011c3b9dadff3be5d0956b64
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-05 19:47:51 +1000

    Time: 71 ms (20.76%), Space: 13.8 MB (64.75%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,6 +1,4 @@
-UPPER = [i for i in str(pow(2, 31)-1)]
-LOWER = UPPER[:-1] + ['8']
-BOUNDARY = [i for i in str(pow(2, 31)-1)]
+
 class Solution:
     BOUNDARY = [i for i in str(pow(2, 31)-1)]
     def reverse(self, x: int) -> int:
@@ -8,33 +6,21 @@ class Solution:
         equal = True
         str_x = str(abs(x))
         len_x = len(str_x)
-        if len_x < len(UPPER):
-            num = int(''.join([i for i in str_x])[-1::-1])
-            num = -num if x<0 else num
-            return num
+        if len_x < len(self.BOUNDARY):
+            s = ''.join([i for i in str_x])[-1::-1]
         else:
             s = ''
             if x<0:
                 self.BOUNDARY = self.BOUNDARY[:-1] + ['8']
             for i, n in enumerate(str_x[-1::-1]):
-                
                 s += n
-                if n == BOUNDARY[i]:
+                if n == self.BOUNDARY[i]:
                     equal = True
-                elif n>BOUNDARY[i]:
+                elif n>self.BOUNDARY[i]:
                     if equal and not smaller:
                         return 0
-                elif n<BOUNDARY[i]:
+                elif n<self.BOUNDARY[i]:
                     smaller = True
                     equal = False
-                #else:
-                #    s += n
-                #    if n == LOWER[i]:
-                #        equal = True
-                #    elif n>LOWER[i]:
-                #        if equal and not smaller:
-                #            return 0
-                #    elif n<LOWER[i]:
-                #        smaller = True
-                #        equal = False
-            return int(s) if x>=0 else -int(s)
+                    
+        return int(s) if x>=0 else -int(s)
