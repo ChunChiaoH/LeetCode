3219ae273cdc5fa552af63de3a01a5e8401c6c43
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-05 17:02:36 +1000

    Time: 101 ms (43.19%), Space: 13.8 MB (76.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,6 @@
 I, X, C, M = 'I', 'X', 'C', 'M'
 V, L, D = 'V', 'L', 'D'
-nums = {I: 1, V: 5, X:10, L:50, C:100, D:500, M: 1000, None: -1}
+nums = {I: 1, V: 5, X:10, L:50, C:100, D:500, M: 1000}
 
 class Solution:
 
@@ -12,13 +12,14 @@ class Solution:
             cur_num = s[i]
             next_num = s[i+1] if i<len_s-1 else None
 
-            #if cur_num in [I, X, C] and next_num:
-            if nums[cur_num] < nums[next_num]:
-                count -= nums[cur_num]
-                i += 1
-            else:
-                count += nums[cur_num]
-                i += 1
+            if cur_num in [I, X, C] and next_num:
+                if nums[cur_num] < nums[next_num]:
+                    count -= nums[cur_num]
+                    i += 1
+                    continue
+                    
+            count += nums[cur_num]
+            i += 1
 
         return count
 
