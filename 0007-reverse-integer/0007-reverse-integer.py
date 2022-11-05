6e54d43934695b6a80c86dae8a47e34db9a4ce21
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-05 19:45:45 +1000

    Time: 60 ms (56.20%), Space: 14 MB (16.29%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,12 +1,11 @@
 UPPER = [i for i in str(pow(2, 31)-1)]
 LOWER = UPPER[:-1] + ['8']
-#BOUNDARY = [i for i in str(pow(2, 31))]
+BOUNDARY = [i for i in str(pow(2, 31)-1)]
 class Solution:
-    
+    BOUNDARY = [i for i in str(pow(2, 31)-1)]
     def reverse(self, x: int) -> int:
         greater, smaller = 0, 0
         equal = True
-        #print(LOWER, UPPER)
         str_x = str(abs(x))
         len_x = len(str_x)
         if len_x < len(UPPER):
@@ -15,45 +14,27 @@ class Solution:
             return num
         else:
             s = ''
+            if x<0:
+                self.BOUNDARY = self.BOUNDARY[:-1] + ['8']
             for i, n in enumerate(str_x[-1::-1]):
-                #if greater == 0:
-                #    if n>UPPER[i]:
-                #        greater = True
-                #if smaller == 0:
-                #    if n<UPPER[i]:
+                
+                s += n
+                if n == BOUNDARY[i]:
+                    equal = True
+                elif n>BOUNDARY[i]:
+                    if equal and not smaller:
+                        return 0
+                elif n<BOUNDARY[i]:
+                    smaller = True
+                    equal = False
+                #else:
+                #    s += n
+                #    if n == LOWER[i]:
+                #        equal = True
+                #    elif n>LOWER[i]:
+                #        if equal and not smaller:
+                #            return 0
+                #    elif n<LOWER[i]:
                 #        smaller = True
-                if x>=0:
-                    s += n
-                    if n == UPPER[i]:
-                        equal = True
-                    elif n>UPPER[i]:
-                        if equal and not smaller:
-                            #continue
-                            return 0
-                        else:
-                            continue
-                    elif n<UPPER[i]:
-                        smaller = True
-                        equal = False
-                else:
-                    s += n
-                    if n == LOWER[i]:
-                        equal = True
-                    elif n>LOWER[i]:
-                        if equal and not smaller:
-                            return 0
-                            #continue
-                        else:
-                            continue
-                            #return 0
-                    elif n<LOWER[i]:
-                        smaller = True
-                        equal = False
+                #        equal = False
             return int(s) if x>=0 else -int(s)
-        
-        #print(lower, upper)
-        #if x >= 0:
-        #    num = int(''.join([i for i in str(x)])[-1::-1])
-        #else:
-        #    num = -int(''.join([i for i in str(x)[1:]])[-1::-1])
-        #return num if lower<num<upper else 0
