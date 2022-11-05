ff176d9bc99a37e7ba59ae948a6cad47b5f1a7cf
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-06 00:21:12 +1000

    Time: 132 ms (57.91%), Space: 15.3 MB (14.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         lefts = {}
         for i, num in enumerate(nums):
-            if num in lefts:
-                return [i, lefts[num]]
-            else:
+            if num not in lefts:
                 lefts[target-num] = i
+            else:
+                return [i, lefts[num]]
         
