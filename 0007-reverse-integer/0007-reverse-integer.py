86450e46f2816a6f836592e5718a0808b89e9f61
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-05 20:05:55 +1000

    Time: 71 ms (20.76%), Space: 13.8 MB (97.14%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,19 +3,20 @@ class Solution:
     
     def reverse(self, x: int) -> int:
         if x>0:
-            BOUNDARY = [i for i in str(pow(2, 31)-1)]
+            BOUNDARY = [int(i) for i in str(pow(2, 31)-1)]
         else:
-            BOUNDARY = [i for i in str(pow(2, 31))]
+            BOUNDARY = [int(i) for i in str(pow(2, 31))]
 
         equal, smaller = True, False
         str_x = str(abs(x))
         
         if len(str_x) < len(BOUNDARY):
-            s = ''.join([i for i in str_x])[-1::-1]
+            s = str_x[-1::-1]
         else:
             s = ''
             for i, n in enumerate(str_x[-1::-1]):
                 s += n
+                n = int(n)
                 if n == BOUNDARY[i]:
                     equal = True
                     
