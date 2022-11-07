6d3baf2c7271d8f09232588a72005f889cbe25ac
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-07 10:43:38 +1000

    Time: 85 ms (11.92%), Space: 13.9 MB (88.32%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,8 +3,13 @@ class Solution:
         lens = sorted([(len(s), i) for i, s in enumerate(strs)])
         shortest_len, shortest_index = lens[0]
         shortest_str = strs[shortest_index]
+        
         for i in range(shortest_len):
-            if not all([s[i] == shortest_str[i] for s in strs]):
-                return shortest_str[:i]
+            for s in strs:
+                if s[i] != shortest_str[i]:
+                    return shortest_str[:i]
+            #if not all([s[i] == shortest_str[i] for s in strs]):
+            #    return shortest_str[:i]
+        
         return shortest_str
         
