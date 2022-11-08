dbc2bc22be75e741aa1373eccec1e8dce81b4c1d
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-09 07:09:57 +1000

    Time: 2085 ms (19.03%), Space: 27.4 MB (76.68%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,9 +3,14 @@ class Solution:
         i, j = 0, len(height)-1
         max_volume = 0
         while i < j:
-            max_volume = max(max_volume, min(height[i], height[j])*(j-i))
             if height[i] < height[j]:
+                max_volume = max(max_volume, height[i]*(j-i))
                 i += 1
             else:
+                max_volume = max(max_volume, height[j]*(j-i))
                 j -= 1
+            #if height[i] < height[j]:
+            #    i += 1
+            #else:
+            #    j -= 1
         return max_volume
