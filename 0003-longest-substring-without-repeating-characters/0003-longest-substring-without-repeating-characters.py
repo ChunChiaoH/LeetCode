8058a093f4e189ac712c72648a26379d94277dd2
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-10 07:43:28 +1000

    Time: 291 ms (19.69%), Space: 14 MB (93.29%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,8 +4,7 @@ class Solution:
             return 0
         l, i = 1, 0
         max_len = 1
-        while l+i < len(s)+1:
-            #print(s[i:i+l], max_len)
+        while i+l < len(s)+1:
             if len(set(s[i:i+l])) == l:
                 max_len = max(max_len, l)
                 l += 1
