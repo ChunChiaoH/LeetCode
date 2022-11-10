e21d4a54531faaea4f47623d6eb157959b688024
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-10 21:58:54 +1000

    Time: 2152 ms (39.70%), Space: 28.2 MB (35.69%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -9,10 +9,14 @@ class Solution:
                 stack.pop()
                 
             stack += [[t, i]]
-            if stack[-2][0] == 101:
-                count += [0]
-            else:
+            if len(stack) > 2:
                 count += [stack[-1][1] - stack[-2][1]]
+            else:
+                count += [0]
+            #if stack[-2][0] == 101:
+            #    count += [0]
+            #else:
+            #    count += [stack[-1][1] - stack[-2][1]]
         count.reverse()
         return count
                 
