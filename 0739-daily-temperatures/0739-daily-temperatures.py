bd35d64603ce8b995f8a6e7161d6d17b522d00b4
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-10 21:57:24 +1000

    Time: 1558 ms (60.75%), Space: 28.3 MB (35.69%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # Monotonic stack
+        stack = [[101, 0]]
+        count = []
+        temperatures.reverse()
+        for i, t in enumerate(temperatures, 1):
+            while t >= stack[-1][0]:
+                stack.pop()
+                
+            stack += [[t, i]]
+            if stack[-2][0] == 101:
+                count += [0]
+            else:
+                count += [stack[-1][1] - stack[-2][1]]
+        count.reverse()
+        return count
+                
