e08c58afb0f5ab63a3df9aff2d55e0ea508eb19e
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-14 14:27:07 +1000

    Time: 54 ms (87.39%), Space: 17.1 MB (65.27%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,9 +1,6 @@
 class Solution:
-    
     lookup = {1:'0'}
     lookup_inv = {1:'1'}
-    def invert(self, s: str) -> str:
-        return ''.join(['0' if ch == '1' else '1' for ch in s])
     def binary_invstr(self, n: int) -> str:
         if n in self.lookup_inv:
             return self.lookup_inv[n]
@@ -14,13 +11,8 @@ class Solution:
         if n in self.lookup:
             return self.lookup[n]
         else:
-            #binary = self.binary_str(n-1)
-            #binary_inv = self.invert(binary)
-            self.lookup[n] = self.binary_str(n-1) + '1' + self.binary_invstr(n-1)#binary + '1' + binary_inv[::-1]
-            #self.lookup_inv[n] = binary + '0' + binary_inv[::-1]
+            self.lookup[n] = self.binary_str(n-1) + '1' + self.binary_invstr(n-1)
             return self.lookup[n]
     def findKthBit(self, n: int, k: int) -> str:
         s = self.binary_str(n)
-        #print(self.lookup)
-        #print(self.lookup_inv)
         return s[k-1]
