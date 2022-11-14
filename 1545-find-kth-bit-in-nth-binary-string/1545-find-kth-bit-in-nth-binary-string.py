2d2371a71c1530beafd16d77cbe948952ed87c51
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-11-14 14:26:04 +1000

    Time: 31 ms (98.67%), Space: 16.9 MB (66.37%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -4,14 +4,20 @@ class Solution:
     lookup_inv = {1:'1'}
     def invert(self, s: str) -> str:
         return ''.join(['0' if ch == '1' else '1' for ch in s])
-    def binary_str(self, n) -> str:
+    def binary_invstr(self, n: int) -> str:
+        if n in self.lookup_inv:
+            return self.lookup_inv[n]
+        else:
+            self.lookup_inv[n] = self.binary_str(n-1) + '0' + self.binary_invstr(n-1)
+            return self.lookup_inv[n]
+    def binary_str(self, n: int) -> str:
         if n in self.lookup:
             return self.lookup[n]
         else:
-            binary = self.binary_str(n-1)
-            binary_inv = self.invert(binary)
-            self.lookup[n] = binary + '1' + binary_inv[::-1]
-            self.lookup_inv[n] = binary_inv + '0' +binary[::-1]#binary_inv[::-1] +'0' + binary[::-1]
+            #binary = self.binary_str(n-1)
+            #binary_inv = self.invert(binary)
+            self.lookup[n] = self.binary_str(n-1) + '1' + self.binary_invstr(n-1)#binary + '1' + binary_inv[::-1]
+            #self.lookup_inv[n] = binary + '0' + binary_inv[::-1]
             return self.lookup[n]
     def findKthBit(self, n: int, k: int) -> str:
         s = self.binary_str(n)
