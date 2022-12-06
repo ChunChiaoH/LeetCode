3116063fb0cf1f092380faff674f39914bf9b5a2
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2022-12-06 23:10:37 +1100

    Time: 1107 ms (19.70%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,13 +1,4 @@
 # Write your MySQL query statement below
-select employee_id, salary as bonus
+select employee_id, salary*(employee_id%2!=0)*(name not like 'M%') as bonus
 from Employees
-where employee_id%2!=0 and name not like 'M%'
-
-union
-
-select employee_id, 0
-from Employees
-where (employee_id%2!=0 and name like 'M%') or
-      (employee_id%2=0 and name not like 'M%') or
-      (employee_id%2=0 and name like 'M%')
 order by employee_id
