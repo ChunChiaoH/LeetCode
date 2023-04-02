dd0018f96f1d42d4c8638a924f0ab60c4d41cdd3
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-04-02 16:03:51 +1000

    Time: 1178 ms (98.58%), Space: 36.7 MB (98.30%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,13 +1,22 @@
 import bisect
 class Solution:
+    #def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+    #    def ceil(a: int, b: int):
+    #        return -(a//-b)
+    #    potions.sort()
+    #    needs = [ceil(success, spell) for spell in spells]
+    #    result = [len(potions)-bisect.bisect_left(potions, need) for need in needs]
+    #    return result
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         def ceil(a: int, b: int):
-            #print(num, -round(-num))
             return -(a//-b)
         potions.sort()
-        #print(potions)
         needs = [ceil(success, spell) for spell in spells]
-        #print(needs)
-        result = [len(potions)-bisect.bisect_left(potions, need) for need in needs]
+        result=[]
+        cache={}
+        for need in needs:
+            if need not in cache:
+                cache[need]=len(potions)-bisect.bisect_left(potions, need)
+            result.append(cache[need])
         return result
         
