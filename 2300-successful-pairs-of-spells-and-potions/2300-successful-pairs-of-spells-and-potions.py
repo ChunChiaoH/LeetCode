bd153a0d9760a47bb5ed5403aa978b12318c1a75
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-04-02 16:04:10 +1000

    Time: 1196 ms (95.47%), Space: 36.6 MB (98.30%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,12 +1,5 @@
 import bisect
 class Solution:
-    #def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-    #    def ceil(a: int, b: int):
-    #        return -(a//-b)
-    #    potions.sort()
-    #    needs = [ceil(success, spell) for spell in spells]
-    #    result = [len(potions)-bisect.bisect_left(potions, need) for need in needs]
-    #    return result
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         def ceil(a: int, b: int):
             return -(a//-b)
