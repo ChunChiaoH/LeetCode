60e3c420356aa5d2b6ad185155d9a1aee2890dc9
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-11-01 00:15:50 +1000

    Time: 176 ms (81.98%), Space: 27.8 MB (46.68%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,13 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #print('***', len(nums))
-        temp = list(nums)
-        for _ in range(k):
-            cur_num = temp.pop()
-            temp = [cur_num]+temp
-            #print(nums)
-        #print('***', len(nums))
+        k = k % len(nums)
+        temp = [n for n in nums[-k:]] + [n for n in nums[:-k]]
+        
         for i, n in enumerate(temp):
-            #print(i)
             nums[i] = n
