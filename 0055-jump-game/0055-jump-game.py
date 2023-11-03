9637d34d076c19c353e487fcb116e3a882c2f7eb
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-11-03 19:13:21 +1000

    Time: 483 ms (22.11%), Space: 17.4 MB (93.49%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -5,15 +5,12 @@ class Solution:
                 return True
             return False
         
-        prev0 = 0
         for i, num in enumerate(nums):
             if num == 0:
                 temp = nums[:i]
                 for j, t in enumerate(temp):
                     if t+j > i or (t+j == i and i == len(nums)-1):
-                        print(i, j)
                         break
                     if j == len(temp)-1:
-                        print(i, j)
                         return False
         return True
