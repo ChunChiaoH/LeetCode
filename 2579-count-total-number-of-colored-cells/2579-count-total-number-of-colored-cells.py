e23e683307b28b6b86b8e42f8caebe9388e59cd1
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-11-07 18:42:31 +1000

    Time: 208 ms (19.07%), Space: 20 MB (9.71%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -2,7 +2,4 @@ class Solution:
     def coloredCells(self, n: int) -> int:
         # 1, 5, 13, 25, 41
         #   4  8  12  16
-        s = 0
-        for i in range(n-1):
-            s += 4*(i+1)
-        return s+1
+        return 1 + sum([4*(i+1) for i in range(n-1)])
