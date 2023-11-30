b3a7c6d7001331a0c622759c1f9baff3fc374a88
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-11-30 20:35:50 +1100

    Time: 33 ms (83.83%), Space: 16.2 MB (73.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # action space: [1, 2]
-        # current state: current position
+        # current state: dp, number of ways from current position
         
+        # range(n+2) to handle situation where n<=2
         dp = [0 for _ in range(n+2)]
         dp[1] = 1
         dp[2] = 2
         if n <= 2:
             return dp[n]
-        #print(n+1)
         for i in range(3, n+1):
             for a in [1, 2]:
                 dp[i] += dp[i-a]
-        #print(dp)
         return dp[n]
