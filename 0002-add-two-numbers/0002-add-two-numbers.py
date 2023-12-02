c2e7029e775d2d20de5243052816c745078656fc
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-02 11:18:53 +1000

    Time: 77 ms (7.34%), Space: 16.5 MB (11.47%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,6 +8,7 @@ class Solution:
         # create another ListNode for sum
         new_node = ListNode(0, None)
         cur_node = new_node
+        
         # the value to add to next bit
         add_to_next = 0
         
@@ -15,23 +16,25 @@ class Solution:
         while l1 or l2:
             l2v = l2.val if l2 else 0
             l1v = l1.val if l1 else 0
-            # sum of l1 and l2
+            
+            # sum the value of l1, l2 and "the other ListNode"
             add_to_cur = (l1v + l2v + cur_node.val)%10
             add_to_next = (l1v + l2v + cur_node.val)//10
-            
             cur_node.val = add_to_cur
-            # cur_node.val may be one or zero depending on previous loop
-            #cur_node.val += add_to_cur
-            #cur_node.val += (cur_node.val + add_to_cur) % 10
-            #add_to_next += cur_node.val//10
-            #cur_node.val = cur_node.val%10
+            
+            # extend "the other ListNode"
             if (l1 and l1.next) or (l2 and l2.next):
                 cur_node.next = ListNode(add_to_next, None)
             else:
                 cur_node.next = None
+            
+            # move forward
             l1 = l1.next if l1 and l1.next != None else None 
             l2 = l2.next if l2 and l2.next != None else None
             cur_node = cur_node.next if cur_node.next else cur_node
+        
+        # in case there is a last bit need to be concatenated
         if add_to_next:
             cur_node.next = ListNode(add_to_next, None)
+            
         return new_node
