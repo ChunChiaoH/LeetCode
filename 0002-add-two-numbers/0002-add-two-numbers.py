8c9d27c90aae04df7a2050f63ec841e5b2c9d7dc
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-02 11:12:52 +1000

    Time: 67 ms (46.02%), Space: 16.4 MB (11.47%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,18 +5,26 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # create another ListNode for sum
         new_node = ListNode(0, None)
         cur_node = new_node
+        # the value to add to next bit
         add_to_next = 0
+        
+        # iterate until both l1 and l2 are exhausted
         while l1 or l2:
             l2v = l2.val if l2 else 0
             l1v = l1.val if l1 else 0
-            add_to_cur = (l1v + l2v)%10
-            add_to_next = (l1v + l2v)//10
+            # sum of l1 and l2
+            add_to_cur = (l1v + l2v + cur_node.val)%10
+            add_to_next = (l1v + l2v + cur_node.val)//10
             
-            cur_node.val += add_to_cur
-            add_to_next += cur_node.val//10
-            cur_node.val = cur_node.val%10
+            cur_node.val = add_to_cur
+            # cur_node.val may be one or zero depending on previous loop
+            #cur_node.val += add_to_cur
+            #cur_node.val += (cur_node.val + add_to_cur) % 10
+            #add_to_next += cur_node.val//10
+            #cur_node.val = cur_node.val%10
             if (l1 and l1.next) or (l2 and l2.next):
                 cur_node.next = ListNode(add_to_next, None)
             else:
