0bbf32200b59f56349c152b80c31fc1d7cd6a0c9
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-06 14:05:21 +1000

    Time: 53 ms (35.91%), Space: 16.9 MB (40.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -24,13 +24,40 @@ class Solution:
         #    index1 -= 1
         #found, index2 = binary_search(matrix[index1], target)
         #return found
+        
         # approch 2: binary tree (by artod@Leetcode)
-        row, col = len(matrix)-1 , 0
-        while row >= 0 and col <= len(matrix[0])-1:
-            if matrix[row][col] == target:
+        #row, col = len(matrix)-1 , 0
+        #while row >= 0 and col <= len(matrix[0])-1:
+        #    if matrix[row][col] == target:
+        #        return True
+        #    elif matrix[row][col] < target:
+        #        col += 1
+        #    else: # matrix[row][col] > target
+        #        row -= 1
+        #return False
+        
+        # approch 3: mix of approach 1 and 2
+        def binary_search(nums: List[int], target: int) -> bool:
+            left, right = 0, len(nums)-1
+            while left <= right:
+                mid = (left + right)//2
+                if nums[mid] < target:
+                    left = mid + 1
+                elif nums[mid] > target:
+                    right = mid -1
+                else:
+                    return True
+            return False
+        
+        row, col = 0, len(matrix[0])-1
+        while row <= len(matrix)-1 and col >= 0:
+            cur = matrix[row][col]
+            if cur > target:
+                #col -= 1
+                # instead of search one by one, do binary search of the whole row
+                return binary_search(matrix[row], target)
+            elif cur <target:
+                row += 1
+            else:
                 return True
-            elif matrix[row][col] < target:
-                col += 1
-            else: # matrix[row][col] > target
-                row -= 1
         return False
