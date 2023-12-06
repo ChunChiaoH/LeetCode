ac30dd5c391bc07afbea260db289bc5e3f562939
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-06 20:07:32 +1000

    Time: 2578 ms (15.52%), Space: 17.7 MB (10.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -7,7 +7,7 @@ class Solution:
         
         for dis, num in enumerate(nums[::-1]):
             step = last_idx - dis
-            if dp[step]:
+            if dp[step] or num == 0:
                 continue
             for n in range(num, 0, -1):
                 if dp[n+step] == True:
