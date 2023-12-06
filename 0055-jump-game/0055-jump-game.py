693105da2f42c7aacc0e47953f2e6c5a5769c82b
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-06 19:52:13 +1000

    Time: 2570 ms (15.59%), Space: 17.6 MB (17.44%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,7 +1,21 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        #dp = [False for _ in nums]
-        #last_idx = len(nums) -1
+        last_idx = len(nums) -1
+        dp = [True if i+num >= last_idx else False for i, num in enumerate(nums)]
+        if dp[0]:
+            return True
+        
+        for dis, num in enumerate(nums[::-1]):
+            step = last_idx - dis
+            if dp[step]:
+                continue
+            for n in range(1, num+1):
+                if dp[n+step] == True:
+                    dp[step] = True
+                    break
+        return dp[0]
+            
+        #print(dp)
         #for i, num in enumerate(nums[::-1]):
         #    if num >= i:
         #        dp[i] = True
@@ -10,23 +24,19 @@ class Solution:
         #        dp[i] = any([dp[i-j] for j in range(1, num+1)])
         #return dp[-1]
         
-        if nums[0] == 0:
-            if len(nums) == 1:
-                return True
-            return False
-    
-        for i, num in enumerate(nums):
-            #print(num + i >= len(nums))
-            #print(num, i, len(nums))
-            if num + i >= len(nums)-1:
-                #print(num + i >= len(nums)-1)
-                #print(num + i, len(nums)-1)
-                return True
-            if num == 0:
-                temp = nums[:i]
-                for j, t in enumerate(temp):
-                    if t+j > i:
-                        break
-                    if j == len(temp)-1:
-                        return False
-        return False
+        #if nums[0] == 0:
+        #    if len(nums) == 1:
+        #        return True
+        #    return False
+    #
+        #for i, num in enumerate(nums):
+        #    if num + i >= len(nums)-1:
+        #        return True
+        #    if num == 0:
+        #        temp = nums[:i]
+        #        for j, t in enumerate(temp):
+        #            if t+j > i:
+        #                break
+        #            if j == len(temp)-1:
+        #                return False
+        #return False
