b81e5cb1225e686b1ac4b1c44caf9164556bab95
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-06 19:55:09 +1000

    Time: 2464 ms (15.96%), Space: 17.8 MB (10.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -9,34 +9,9 @@ class Solution:
             step = last_idx - dis
             if dp[step]:
                 continue
-            for n in range(1, num+1):
+            for n in range(num, 0, -1):
                 if dp[n+step] == True:
                     dp[step] = True
                     break
+                    
         return dp[0]
-            
-        #print(dp)
-        #for i, num in enumerate(nums[::-1]):
-        #    if num >= i:
-        #        dp[i] = True
-        #    else:
-        #        # current index - [1, num]
-        #        dp[i] = any([dp[i-j] for j in range(1, num+1)])
-        #return dp[-1]
-        
-        #if nums[0] == 0:
-        #    if len(nums) == 1:
-        #        return True
-        #    return False
-    #
-        #for i, num in enumerate(nums):
-        #    if num + i >= len(nums)-1:
-        #        return True
-        #    if num == 0:
-        #        temp = nums[:i]
-        #        for j, t in enumerate(temp):
-        #            if t+j > i:
-        #                break
-        #            if j == len(temp)-1:
-        #                return False
-        #return False
