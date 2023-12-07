ca3ba05be32c4e43543da557f44ddbf8691475c1
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-07 12:45:38 +1000

    Time: 42 ms (29.22%), Space: 16.4 MB (9.79%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -8,7 +8,9 @@ class Solution:
         longer = max(len(bin(start)), len(bin(goal))) - 2
         count = 0
         for _ in range(longer):
+            # get the right-most bit and XOR
             count = count+1 if (start&1) ^ (goal&1) else count
+            # shift both one bit to right
             start >>= 1
             goal >>= 1
         return count
