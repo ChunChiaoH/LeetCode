c3e18995b0edce35fea4a2433c9a9d7b28ad380c
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-08 13:47:15 +1000

    Time: 74 ms (11.06%), Space: 18.7 MB (6.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -6,19 +6,13 @@ class Solution:
         dp = [0 for _ in prices]
         dp[-1] = 0
         dp[-2] = max(0, prices[-1] - prices[-2])
+        
+        max_price = max(prices[-1], prices[-2])
         profit = 0
         idxs = [i for i in range(len(prices)-2)][::-1]
         for idx, p in zip(idxs, prices[::-1][2:]):
-            dp[idx] = max(prices[idx+1]-prices[idx], 0) + dp[idx+1]
-        
-        return dp[0]
-            #print(idx, p)
-            # 5 4
-            # 4 6
-            # 3 3 start from here
-            # 2 5
-            # 1 1
-            # 0 7
+            #dp[idx] = max(prices[idx+1]-prices[idx], 0) + dp[idx+1]
+            max_price = max(p, max_price)
+            dp[idx] = max(p-max_price, max(prices[idx+1]-prices[idx], 0) + dp[idx+1])
             
-        
-        
+        return dp[0]
