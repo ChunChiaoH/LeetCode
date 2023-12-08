0eeb55fcd93fbf0a861e00db80d7dc6d6c3b3137
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-08 11:29:16 +1000

    Time: 37 ms (77.25%), Space: 16.3 MB (55.17%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -2,13 +2,18 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         tri = []
         
-        for i in range(numRows):
-            row = []
-            for j in range(i+1):
-                if j == 0 or j == i:
-                    row.append(1)
+        for n in range(numRows):
+            half = []
+            for j in range((n//2)+1):
+                if j == 0:
+                    half.append(1)
                 else:
-                    row.append(tri[i-1][j] + tri[i-1][j-1])
+                    half.append(tri[n-1][j] + tri[n-1][j-1])
+                    
+            if n%2 == 0:
+                row = half + half[::-1][1:]
+            else: #n%2 == 1
+                row = half + half[::-1]
             tri.append(row)
             
         return tri
