2a5dadd8475a2b87a95807d0d1b63bd47f7bfce9
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-15 15:28:03 +1000

    Time: 46 ms (43.04%), Space: 16.3 MB (39.80%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,17 +7,21 @@ class Solution:
             return nums1
         
         for i, n1 in enumerate(nums1):
+            # iterate through nums1 to find inserting point
             tmp = []
             while nums2:
+                # insert as much as possible at one time
                 if nums2[0] <= n1:
                     tmp.append(nums2[0])
                     nums2 = nums2[1:]
                 else:
                     break
-            #print(nums1, tmp, nums2, '*')
+            
             if tmp:
+                # insert
                 nums1[:] = nums1[:i] + tmp + nums1[i:-len(tmp)]
+                
         if nums2:
-            nums1[:] = nums1[:-len(nums2)] + nums2 
-            #print(nums1, tmp, nums2)
+            # if any of nums2 left
+            nums1[:] = nums1[:-len(nums2)] + nums2
         
