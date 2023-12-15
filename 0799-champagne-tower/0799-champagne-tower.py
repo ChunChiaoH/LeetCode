ae5a6d0a3625f9380f79be479c8a8a3b3196ca82
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-15 14:33:51 +1000

    Time: 140 ms (22.61%), Space: 16.3 MB (82.28%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,10 +1,5 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        #           p9
-        #            8
-        #           3 3
-        #         .5 2 .5
-        #        0  1 1  0
         if poured == 0:
             return 0
         if query_row == 0:
@@ -12,26 +7,24 @@ class Solution:
         
         prev_row = [poured - 1]
         cur_row = []
-        queried = []
         for i in range(1, query_row+1):
             num_glasses = i+1
             cur_row = [0 for _ in range(num_glasses)]
             for j in range(num_glasses):
-                if j == 0:
+                if j == 0: # left-most glass
                     cur_row[j] = prev_row[j]/2
-                elif j == num_glasses-1:
+                elif j == num_glasses-1: # right-most glass
                     cur_row[j] = prev_row[j-1]/2
                 else:
                     cur_row[j] = prev_row[j-1]/2 + prev_row[j]/2
-                
-                #if i == query_row:
-                #    queried += [1] if cur_row[j] >= 1 else [cur_row[j]]
+                    
                 if i!= query_row:
+                    # transfer to throughput if it's not the queried row
                     cur_row[j] = max(cur_row[j]-1, 0)
-                #else:
-                #    cur_row[j] = min(cur_row[i], 1)
+                # else, cur_row[j] stores how much has been pored into it
+                # this is only for the queried row,
+                # such that when amount been poured into cur_row[j] is less than 1 can be kept
            
             prev_row = cur_row
             
-        
         return min(cur_row[query_glass], 1)
