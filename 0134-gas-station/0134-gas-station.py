8a352fc7c3df1a1ada704dabde52e3311d74631d
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-16 15:00:14 +1000

    Time: 1084 ms (33.50%), Space: 21.6 MB (98.12%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -8,25 +8,32 @@ class Solution:
         #  4  4  1  5  1
         #  1 -3  1 -2  3
         
+        # approach 1: 
         left = [g-c for g, c in zip(gas, cost)]
-        #if sum(left) < 0:
-        #    return -1
+        if sum(left) < 0:
+            return -1
         
         j = 0
         cum_sum = 0
-        l_cum_sum = 0
         for i, l in enumerate(left):
             cum_sum += l
             if cum_sum < 0:
-                l_cum_sum += cum_sum
                 cum_sum = 0
                 j = i+1
-        
-        #return j
-        #print(left)
-        #print(l_cum_sum, cum_sum)
-        if l_cum_sum + cum_sum >= 0:
-            return j
-        else:
-            return -1
+                
+        return j
+    
+        # approach 2: 
+        #left = [g-c for g, c in zip(gas, cost)]
+        #
+        #j = 0
+        #cum_sum = l_cum_sum = 0
+        #for i, l in enumerate(left):
+        #    cum_sum += l
+        #    if cum_sum < 0:
+        #        l_cum_sum += cum_sum
+        #        cum_sum = 0
+        #        j = i+1
+        #
+        #return j if l_cum_sum + cum_sum >= 0 else -1
                 
