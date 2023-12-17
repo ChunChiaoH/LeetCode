83d1e2fed719a745b9db1c932b5101e935215082
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-17 17:22:33 +1000

    Time: 151 ms (31.99%), Space: 19.7 MB (9.45%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,25 +1,19 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         # sliding window
-        # s  : 1 0 2
-        # c1 : 2 1 2
-        # c2 : 2 1 2
         
-        # s  : 1 2 2
-        # c1 : 1 2 1
-        # c2 : 1 2 1
-        
-        # s  : 5 2 3 1 3 5
-        # c1 : 1 1 2 1 2 3
-        # c2 : 2 1 2 1 2 3
         
         candy = [1 for _ in ratings]
         idx = [i for i in range(len(ratings))]
         
+        # going ->
+        # compare the right with the left
         for i, r in zip(idx[1:], ratings[1:]):
             if r > ratings[i-1] and candy[i] <= candy[i-1]:
                 candy[i] = candy[i-1] + 1
-                
+        
+        # goring <-
+        # compare the left with the right
         for i, r in zip(idx[::-1][1:], ratings[::-1][1:]):
             if r > ratings[i+1] and candy[i] <= candy[i+1]:
                 candy[i] = candy[i+1] + 1
