4b397ccc8eb56661d610162e82348c01ab210215
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2023-12-19 14:01:50 +1000

    Time: 75 ms (6.23%), Space: 17.1 MB (77.36%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = 'a'+s+'a'
+        n = len(s)
+        i, j = 0, n-1
+        res = True
+        
+        def isAlphanumeric(c: str) -> bool:
+            if ord('A') <= ord(c) <= ord('Z'):
+                return True
+            if ord('a') <= ord(c) <= ord('z'):
+                return True
+            if ord('0') <= ord(c) <= ord('9'):
+                return True
+            
+            return False
+        
+        while i < j:
+            while not isAlphanumeric(s[i]):
+                i += 1
+            while not isAlphanumeric(s[j]):
+                j -= 1
+            
+            if s[i].lower() == s[j].lower():
+                i += 1
+                j -= 1
+            else:
+                return False
+        
+        if i == j and s[i] != s[j]:
+            return False
+        return True
