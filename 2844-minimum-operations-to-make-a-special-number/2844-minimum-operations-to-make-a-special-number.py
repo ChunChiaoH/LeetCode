6a2ffbff82ec14efc10b7f65214dd40e37f5b418
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2024-06-15 14:22:29 +1000

    Time: 31 ms (94.55%), Space: 16.5 MB (86.67%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.py
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.py
@@ -3,63 +3,28 @@ class Solution:
         if len(num) == 1:
             return 1 if num != '0' else 0
 
-        #min_operations = {s: len(num) for s in ['00', '25', '50', '75']}
-        ## min_operations: {'00': len(num), '25': len(num), ...}
-        #
-        #all_digits = {d: [i for i, n in enumerate(num[::-1]) if n== d] 
-        #      for d in set("".join([k for k in min_operations.keys()]))}
-        ## all_digits: {'0': [indices of 0s in num], '2': indices of 2s in num, ...}
-        #
-        #for k in min_operations:
-        #    first_digit = all_digits[k[0]]
-        #    second_digit = all_digits[k[1]]
-        #    if (not first_digit) or (not second_digit):
-        #        continue
-        #    for d in first_digit:
-        #        if d > min(second_digit):
-        #            min_operations[k] = min(min_operations[k], d-1)
-        #            break
-        #            
-        #least_o = min(min_operations.values())
-        #if least_o == len(num):
-        #    return least_o-1 if len(all_digits['0']) == 1 else least_o
-        #else:
-        #    return least_o
-
         rev_num = num[::-1]
         has_0 = has_5 = False
-        count = len(rev_num)
-        i = j = 0
+        min_ops = len(rev_num)
         
-        while i < len(rev_num):
+        for i, digit in enumerate(rev_num):
             if not has_0:
-                if rev_num[i] == '0':
+                if digit == '0':
                     has_0 = True
             else:
-                if rev_num[i] == '0' or rev_num[i] == '5':
-                    count = min(count, i-1)
-            i += 1
-            #if rev_num[i] == '0' and not has_0:
-            #    has_0 = True
-            #    i += 1
-            #    continue
-            #    
-            #if (rev_num[i] == '5' or rev_num[i] == '0') and has_0:
-            #    count = min(count, i-1)
-            #i += 1
-           
-        while j< len(rev_num):
-            if rev_num[j] == '5' and not has_5:
-                j += 1
-                has_5 = True
-                continue
-                
-            if (rev_num[j] == '7' or rev_num[j] == '2') and has_5:
-                count = min(count, j-1)
-            j += 1
+                if digit == '0' or digit == '5':
+                    min_ops = min(min_ops, i-1)
+                    
+        for i, digit in enumerate(rev_num):
+            if not has_5:
+                if digit == '5':
+                    has_5 = True
+            else:
+                if digit == '2' or digit == '7':
+                    min_ops = min(min_ops, i-1)
         
-        if count == len(rev_num) and has_0:
-            return count -1
+        if min_ops == len(rev_num) and has_0:
+            return min_ops -1
         else:
-            return count
+            return min_ops
         
