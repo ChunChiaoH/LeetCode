09fa006502ba1682c6f30a5d970af30f97887e35
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2024-06-15 16:11:08 +1000

    Time: 239 ms (11.36%), Space: 17.6 MB (83.33%) - LeetHub

--- 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.py
+++ 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.py
@@ -8,14 +8,13 @@ class Solution:
             freq1[d] = 1 if d not in freq1 else freq1[d]+1
         for d in word2:
             freq2[d] = 1 if d not in freq2 else freq2[d]+1
-        #if len(freq1) == len(freq2):
-        #    return True
         
-        all_alphabets = list(set(word1 + word2))
-        for i, a1 in enumerate(all_alphabets):
+        all_alphabets = sorted(list(set(word1 + word2)))
+        for a1 in all_alphabets:
             for a2 in all_alphabets:
                 if (a1 not in freq1) or (a2 not in freq2):
                     continue
+                # swap a1(from word1) with a2(from word2)
                 freq1[a1] -= 1
                 freq2[a1] = freq2[a1]+1 if a1 in freq2 else 1
                 freq1 = {k:v for k, v in freq1.items() if v>0}
@@ -23,19 +22,19 @@ class Solution:
                 freq1[a2] = freq1[a2]+1 if a2 in freq1 else 1
                 freq2[a2] -= 1
                 freq2 = {k:v for k, v in freq2.items() if v>0}
-                    
+                
                 if len(freq1) == len(freq2):
-                    print(a1, a2)
-                    print(freq1)
-                    print(freq2)
                     return True
                 
+                # swap back
                 freq1[a1] = freq1[a1]+1 if a1 in freq1 else 1
                 freq2[a1] = freq2[a1]-1
                 freq2 = {k:v for k, v in freq2.items() if v>0}
+                
                 freq1[a2] = freq1[a2]-1
                 freq2[a2] = freq2[a2]+1 if a2 in freq2 else 1
                 freq1 = {k:v for k, v in freq1.items() if v>0}
+                
         return False
                 
                     
