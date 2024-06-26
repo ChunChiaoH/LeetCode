c655b9a007036f1e8d152d367ade5b4f451be642
ChunChiaoH <110368427+ChunChiaoH@users.noreply.github.com>
2024-06-26 13:15:26 +1000

    Time: 54 ms (98.30%), Space: 16.6 MB (93.91%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        size_map = {}
+        result = []
+        for i, s in enumerate(groupSizes):
+            if s in size_map:
+                size_map[s].append(i)
+            else:
+                size_map[s] = [i]
+            #size_map[s] = size_map[s].append(i) if s in size_map else [i]
+            if len(size_map[s]) == s:
+                result.append(size_map[s])
+                del size_map[s]
+        return result
